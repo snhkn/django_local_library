ee3bb6198610d106d22a44b2d18e72d64c68377a
snhkn <senihaakin@gmail.com>
2025-04-30 18:22:27 +0300

    set up django admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Genre, Author, Book, BookInstance, Language
 
-# Register your models here.
+admin.site.register(Genre)
+
+admin.site.register(Language)
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
+
+    def display_genre(self, obj):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in obj.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+
+admin.site.register(Book, BookAdmin)
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+admin.site.register(BookInstance, BookInstanceAdmin)
