03e137fc2f6fa12cd5630c270ae7f48faee44abe
snhkn <senihaakin@gmail.com>
2025-04-30 20:20:10 +0300

    Added home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    # Books that contain a in the name
+    num_books_with_a = Book.objects.filter(title__icontains='a').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_genres': num_genres,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_books_with_a': num_books_with_a,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
